740a7a9b03d94be13f944195eeea46983c62e71a
Alexis Bertin <abertin@la-joliverie.com>
2021-11-22 15:42:48 +0100

    projet de base

---
+++ app/src/main/java/com/example/a2slam_2021_p2_eq5/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.a2slam_2021_p2_eq5;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
