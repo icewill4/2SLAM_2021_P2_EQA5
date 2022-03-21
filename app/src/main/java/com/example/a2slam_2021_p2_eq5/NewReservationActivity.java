c8c95b46055182675e4e01708eae49a47a03ba13
Lucie BLANLOEIL <lblanloeil@la-joliverie.com>
2022-03-21 17:01:14 +0100

    Reservation

--- app/src/main/java/com/example/a2slam_2021_p2_eq5/NewReservationActivity.java
+++ app/src/main/java/com/example/a2slam_2021_p2_eq5/NewReservationActivity.java
@@ -8,7 +8,7 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
-public class NewClientActivity extends Activity {
+public class NewReservationActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_reservationresto);
